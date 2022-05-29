cd39116160c6136df4a0438268060bfbb2f6bfb9
Nurmuhammad <sobirovn707@gmail.com>
2022-05-29 23:11:26 +0500

    usepaginate

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
 import "./App.css";
-import React, { createContext, useState } from "react";
+import React, { createContext } from "react";
 import UseLocalStorage from "./components/UseLocalStorage";
 import UseApi from "./components/useApi";
-import UsePaginate from "./components/UsePaginate";
-import useApi from './hooks/useApi';
 
 export const AppContext = createContext(null)
 const App = () => {
-  const { data, loading, error } = useApi(
-    "https://jsonplaceholder.typicode.com/posts?_limit=32"
-  );
-
-  const [items, $items] = useState([])
-
-  function paginate(array, pageSize, page_number) {
-    return array.slice((page_number - 1) * pageSize, page_number * pageSize);
-  }
 
   return (
     <div className="container">
       <UseLocalStorage />
-
-      <AppContext.Provider value={{ data, loading, error, items, $items, paginate }}>
-        <UseApi />
-        <UsePaginate />
-      </AppContext.Provider>
-      {/* <UseInput />
-      <UseReducerHook />
-      <h3>useRef</h3>
-      <UseRefHook />
-      <UseFetch /> */}
+      <UseApi />
     </div>
   );
 };
